c4fd51e9e047e8bed54cbfb81417e325dd39c889
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-09-23 06:02:02 +0600

    App finished

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import './App.css';
+import Board from './components/Board/Board';
+import Game from './lib/Game';
+import Button from './components/Button/Button';
+import Status from './components/Status/Status';
+import { GameStatus } from './types.d';
+import Counter from './components/Counter/Counter';
+
+const rows = 6,
+  cols = 6;
+
+const game = new Game(rows, cols);
+
+const App = () => {
+  const [board, setBoard] = useState(game.board);
+  const [counter, setCounter] = useState(game.counter);
+  const [finished, setFinished] = useState(game.finished);
+
+  const updateState = () => {
+    setBoard(game.board);
+    setCounter(game.counter);
+    setFinished(game.finished);
+  };
+
+  const onCellClick = (row: number, col: number) => {
+    game.click(row, col);
+    updateState();
+  };
+
+  const onReset = () => {
+    game.reset();
+    updateState();
+  };
+
+  return (
+    <>
+      <div className='card'>
+        <Button caption='New game' onClick={onReset} />
+        <Board rows={rows} cols={cols} board={board} onCellClick={onCellClick} />
+        <Counter counter={counter} />
+        {finished ? <Status status={GameStatus.Won} /> : null}
+      </div>
+    </>
+  );
+};
+
+export default App;
